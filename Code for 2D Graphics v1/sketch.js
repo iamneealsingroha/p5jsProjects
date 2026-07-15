406d7ff2c1e96ec1784e5421b61cf0cfd5e16d84
Neeal Singroha <iamneealsingroha@gmail.com>
2026-07-15 15:57:38 +0530

    first version

---
+++ Code for 2D Graphics v1/sketch.js
@@ -0,0 +1,27 @@
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(200, 240, 255);
+  noStroke();
+  fill(230, 255, 255);
+  circle(0,280,150);
+  circle(100,250, 150);
+  circle(200,200,200);
+  circle(300,200,100);
+  circle(350,250,100);
+  circle(450,300,200);
+  circle(550,250,200);
+  rect(0,250,600,150);
+  fill(255,255,50);
+  circle(450,300,90)
+  fill(150,240,80);
+  circle(250,600,700)
+  fill(150,270,80);
+  circle(540,430,300);
+  fill(150,250,80);
+  circle(350,550,500)
+  fill(150,260,80);
+  circle(50,500,500);
+}

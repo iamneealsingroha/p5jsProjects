e9bce5f005e790421915fa80e0fb2a19e5bdf63d
Neeal Singroha <iamneealsingroha@gmail.com>
2026-07-20 18:04:10 +0530

    I started work on Steve and Alex, uptill now I have only made Steve.

---
+++ Steve and Alex v1/sketch.js
@@ -0,0 +1,33 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(200,255,200);
+  stroke(255,0,0);
+  strokeWeight(3);
+  noStroke();
+  fill(255,255,255);
+  circle(200,200,350);
+  rectMode(CENTER);
+  fill(80,50,40);
+  rect(200,150,150,70);
+  fill(240,200,150);
+  rect(200,220,130,110);
+  rect(200,270,50,50);
+  fill(255,255,255);
+  rect(170,200,30);
+  rect(230,200,30);
+  fill(0,0,0)
+  rect(175,200,15);
+  rect(225,200,15);
+  fill(255,255,255);
+  ellipse(200,240,55,20)
+  fill(49, 230, 239);
+  rect(200,375,200,175);
+  rect(75,308,45,40);
+  rect(325,308,45,40);
+  fill(240, 200, 150);
+  rect(75,375,45,100);
+  rect(325,375,45,100);
+}
